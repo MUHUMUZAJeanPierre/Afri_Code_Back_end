9a3571a4bc2da054285857efd3d233f17af74883
dusengepeggy <dickinepeggy100@gmail.com>
2024-03-27 15:33:36 +0200

    adding cors

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors=require("cors")
 const { Routes } = require("./Routes/StudentRoute");
 const { signUp, signIn } = require("./Controllers/Aunthantication");
 require("dotenv").config()
 const { courseRoute } = require("./Routes/courseRoute");
 
 app.use(express.json());
+
+app.use(cors());
 
 
 app.use('/api/user', Routes);
