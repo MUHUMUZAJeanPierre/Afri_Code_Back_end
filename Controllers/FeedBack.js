3dde670d968445f87f1944db2a3fef28f789229f
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-02 19:04:25 +0200

    mongoose

--- Controllers/FeedBack.js
+++ Controllers/FeedBack.js
@@ -1,8 +1,8 @@
-const FeedbackData = require('../Modules/Contact');
+const FeedbackInfo = require('../Modules/Contact');
 
 const createFeedback = async (req,res,next)=>{
     try {
-        const newFeedback = await  FeedbackData.create(req.body)
+        const newFeedback = await  FeedbackInfo.create(req.body)
         res.status(400).json({message: 'feedBackSent already', newFeedback });
     } catch (error) {
         res.status(500).json({message:'error in sending message', error})
