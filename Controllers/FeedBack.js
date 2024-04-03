c3040c0423bd79c0d6a4e40c7d47318dc5f6ab8c
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-03 13:48:11 +0200

    feeback add,getall and delete actions

--- Controllers/FeedBack.js
+++ Controllers/FeedBack.js
@@ -9,4 +9,23 @@ const createFeedback = async (req,res,next)=>{
     }
 }
 
-module.exports= {createFeedback}
+
+const getAllFeedback= async (req,res,next)=>{
+    try {
+        const feedback = await FeedbackInfo.find({});
+        res.status(200).json({message: "All feedback", feedback});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+const deleteFeedback = async (req,res,next)=>{
+    try {
+        const feedback = await FeedbackInfo.findByIdAndDelete(req.query.id);
+        res.status(200).json({message: "Feedback deleted", feedback});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+module.exports= {createFeedback,getAllFeedback,deleteFeedback}
--- Routes/StudentRoute.js
+++ Routes/StudentRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { signUp, signIn ,getAllStudents,DeleteStudent} = require("../Controllers/Aunthantication.js") ;
+const { createFeedback, getAllFeedback, deleteFeedback } = require('../Controllers/FeedBack.js');
 const Routes = express.Router();
 
 
@@ -9,6 +10,9 @@ Routes.post('/signIn', signIn);
 Routes.get("/getAllStudent",getAllStudents);
 
 Routes.delete("/deleteStudent",DeleteStudent);
+Routes.post("/addFeedback",createFeedback);
+Routes.get ("/allFeedback",getAllFeedback);
+Routes.delete("/deleteFeedback",deleteFeedback)
 
 
 
