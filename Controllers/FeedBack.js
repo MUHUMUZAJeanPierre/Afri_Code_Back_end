cd8b4f59a0eefb778fb57d39cf610958d79e6419
dusengepeggy <dickinepeggy100@gmail.com>
2024-04-04 17:13:14 +0200

    status code changing

--- Controllers/FeedBack.js
+++ Controllers/FeedBack.js
@@ -3,7 +3,7 @@ const FeedbackInfo = require('../Modules/Contact');
 const createFeedback = async (req,res,next)=>{
     try {
         const newFeedback = await  FeedbackInfo.create(req.body)
-        res.status(400).json({message: 'feedBackSent already', newFeedback });
+        res.status(200).json({message: 'feedBackSent already', newFeedback });
     } catch (error) {
         res.status(500).json({message:'error in sending message', error})
     }
